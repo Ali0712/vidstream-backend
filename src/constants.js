398f830dcc0b87dd657dc3a547e1da776cbcc9de
Muhammad Ali <muhammadali07122@gmail.com>
2024-03-11 11:34:28 +0500

    creating a server

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "vidstream";
+export const PORT = process.env.PORT || 8000;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config({path : '.env'})
 import connectDB from './db/dbConnection.js';
+import { app } from './app.js';
+import { PORT } from './constants.js';
+
 
 connectDB()
+.then(() => {
+    app.listen(PORT, ()=>{
+        console.log(`Server is listening on PORT: ${PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB error occured", error)
+})
