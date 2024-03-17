1c7d01a5c23ba2358b7c23a853790371fc45d6f4
Muhammad Ali <muhammadali07122@gmail.com>
2024-03-17 20:19:31 +0500

    added user controller methods

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,30 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {User} from "../models/User.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "user register"
+    // res.status(200).json({
+    //     message: "user register"
+    // })
+
+    const {email, fullName, password, username} = req.body;
+    console.log(email, password)
+
+    if ([email, fullName, password, username].some(field => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
     })
+
+    if (existedUser){
+        throw new ApiError(409, "User already exists")
+    }
+
+    req
+
+
 })
 
 
